07a2a4bbcb1e00daaf4b4685bdbf2a87127208e0
smartnet <ivan@kachinskiy.ru>
2018-09-25 20:12:51 +0300

    Файлы settings.php и settings.local.php контролируем в git

---
+++ web/sites/default/settings.local.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: smartnet
+ * Date: 25.09.18
+ * Time: 0:27
+ */
+
+
+/**
+ * Настройка подключения к локальной базе данных
+ * конфигурации Development
+ */
+$config_directories['sync'] = '../config/sync';
+$databases['default']['default'] = [
+  'database' => 'd8jet',
+  'username' => 'postgres',
+  'password' => '1q2w3e4r@',
+  'prefix' => '',
+  'host' => 'localhost',
+  'port' => '5432',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\pgsql',
+  'driver' => 'pgsql',
+];
+
+/**
+ * Доверенные хосты для конфигурации
+ * Development
+ */
+$settings['trusted_host_patterns'] = [
+    '^d8jet$',
+    '^localhost$',
+];
